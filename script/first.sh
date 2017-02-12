9741edaa7db3cd5f4dd7cfb4203c4c566b0ab0d2
Defn Nghiem <iam@defn.sh>
2017-02-12 11:04:13 -0800

    check systemctl

--- script/first.sh
+++ script/first.sh
@@ -3,12 +3,14 @@
 function main {
   set -exfu
 
-  while true; do
-    case "$(echo | "$@" systemctl is-active cloud-final.service)" in
-      active|failed) break ;;
-      *) echo "Waiting for cloud-init"; sleep 5 ;;
-    esac
-  done
+  if systemctl 2>&1 >/dev/null; then
+    while true; do
+      case "$(echo | "$@" systemctl is-active cloud-final.service)" in
+        active|failed) break ;;
+        *) echo "Waiting for cloud-init"; sleep 5 ;;
+      esac
+    done
+  fi
 
   export DEBIAN_FRONTEND=noninteractive 
 
