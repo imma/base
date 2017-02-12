a45fe1bc3669eaf74ad6efb80d4f836e53ce1cb2
Defn Nghiem <iam@defn.sh>
2017-02-12 13:18:01 -0800

    force evething to use cloud-init

--- script/first.sh
+++ script/first.sh
@@ -13,19 +13,6 @@ function main {
           esac
         done
       fi
-
-      export DEBIAN_FRONTEND=noninteractive 
-
-      dpkg --remove-architecture i386
-
-      export DEBIAN_FRONTEND=noninteractive
-      apt-get install -y software-properties-common python-software-properties openssh-server aptitude
-      env - add-apt-repository -y ppa:git-core/ppa
-      env - add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
-      aptitude purge -y nano mlocate ubuntu-release-upgrader-core update-manager-core
-      aptitude update -y && sudo -E aptitude upgrade -y
-      aptitude install -y net-tools sudo cloud-init git curl unzip perl ruby python language-pack-en build-essential vim man screen tmux
-      aptitude install -y libffi-dev libssl-dev libreadline-dev build-essential zlib1g-dev libxml2-dev libxslt1-dev autoconf automake libtool
       ;;
   esac
 }
