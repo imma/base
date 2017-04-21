3ad6b826367318e3997d9c367134b8e46ba41e62
Defn Nghiem <iam@defn.sh>
2017-04-21 16:37:47 -0700

    guest additions are a pain, just mount with nfs

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -3,13 +3,15 @@
 set -exfu
 
 function main {
+  return 0
+
   echo "==> Installing VirtualBox guest additions"
 
   aptitude install -y build-essential perl dkms
 
   VBOX_VERSION=$(cat /home/ubuntu/.vbox_version)
   mount -o loop /home/ubuntu/VBoxGuestAdditions.iso /mnt
-  sh /mnt/VBoxLinuxAdditions.run
+  sh -x /mnt/VBoxLinuxAdditions.run
   umount /mnt
   rm /home/ubuntu/VBoxGuestAdditions.iso
   rm /home/ubuntu/.vbox_version
