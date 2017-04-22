6402a30af7c49e689e9dcbcfc94bacfb763459ea
Defn Nghiem <iam@defn.sh>
2017-04-21 23:13:24 -0700

    dont install virtualbox guest additions

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -3,15 +3,13 @@
 set -exfu
 
 function main {
-  return 0
+  aptitude install -y build-essential perl dkms
 
   echo "==> Installing VirtualBox guest additions"
 
-  aptitude install -y build-essential perl dkms
-
   VBOX_VERSION=$(cat /home/ubuntu/.vbox_version)
   mount -o loop /home/ubuntu/VBoxGuestAdditions.iso /mnt
-  sh -x /mnt/VBoxLinuxAdditions.run
+  # sh -x /mnt/VBoxLinuxAdditions.run
   umount /mnt
   rm /home/ubuntu/VBoxGuestAdditions.iso
   rm /home/ubuntu/.vbox_version
